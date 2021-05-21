312d31cafc32ce443b2bbc1429b47eb1f6a48ff1
feyaq13 <feyaq13@gmail.com>
2021-05-21 16:43:26 +0300

    реализует фильтр по емейлам

--- 01-basics/02-MarkedList/script.js
+++ 01-basics/02-MarkedList/script.js
@@ -1,7 +1,6 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
-/*
 const emails = [
   'Eliseo@gardner.biz',
   'Jayne_Kuhic@sydney.com',
@@ -29,6 +28,22 @@ const emails = [
   'Jeffery@juwan.us',
   'Isaias_Kuhic@jarrett.net',
 ];
-*/
 
-// Требуется создать Vue приложение
+new Vue({
+  data() {
+    return {
+      emails,
+      valueInput: '',
+    };
+  },
+
+  computed: {
+    matchEmails() {
+      if (this.valueInput) {
+        return emails.filter((email) => email.match(this.valueInput));
+      } else {
+        return [];
+      }
+    },
+  },
+}).$mount('#app');
