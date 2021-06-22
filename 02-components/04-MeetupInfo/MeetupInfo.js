34faa0abc053dede4757ff5d8f7ceaa19d428a58
feyaq13 <feyaq13@gmail.com>
2021-06-22 19:56:38 +0300

    реализует компонент информация о митапе

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,47 @@
 export default {
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      required: true,
+      type: String,
+    },
+    place: {
+      required: true,
+      type: String,
+    },
+    date: {
+      required: true,
+      type: Date,
+    },
+  },
+
+  computed: {
+    dateHuman() {
+      return new Date(this.date).toLocaleString(navigator.language, {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+      });
+    },
+    dateNum() {
+      return new Date(this.date).toISOString().split('T')[0];
+    },
+  },
+
   template: `
     <ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="dateNum"> {{ dateHuman }}</time>
       </li>
     </ul>`,
 };
