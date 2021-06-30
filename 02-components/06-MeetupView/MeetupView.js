3bcf43ee13001b11c47be7c25d45edb5a41ad929
feyaq13 <feyaq13@gmail.com>
2021-06-30 21:02:24 +0300

    реализует отображение страницы на основе сделанных компонентов

--- 02-components/06-MeetupView/MeetupView.js
+++ 02-components/06-MeetupView/MeetupView.js
@@ -1,26 +1,48 @@
-// import MeetupCover from './MeetupCover.js';
-// import MeetupDescription from './MeetupDescription.js';
-// import MeetupAgenda from './MeetupAgenda.js';
-// import MeetupInfo from './MeetupInfo.js';
-// import { getImageUrlByImageId } from './data.js';
+import MeetupCover from '../03-MeetupCover/MeetupCover.js';
+import MeetupDescription from '../02-MeetupDescription/MeetupDescription.js';
+import MeetupAgenda from '../05-MeetupAgenda/MeetupAgenda.js';
+import MeetupInfo from '../04-MeetupInfo/MeetupInfo.js';
+import { getImageUrlByImageId } from './data.js';
 
 const MeetupView = {
   name: 'MeetupView',
 
+  getImageUrlByImageId,
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    date() {
+      return new Date(this.meetup.date);
+    },
+  },
+
+  components: {
+    MeetupDescription,
+    MeetupAgenda,
+    MeetupCover,
+    MeetupInfo,
+  },
+
   template: `
     <div>
-      <!-- meetup cover -->
+     <meetup-cover :title="meetup.title" :link="$options.getImageUrlByImageId(meetup.imageId)" />
       <div class="container">
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
-            <!-- meetup description -->
+            <meetup-description :description="meetup.description" />
 
             <h3>Программа</h3>
-            <!-- meetup agenda -->
+            <meetup-agenda :agenda="meetup.agenda" />
           </div>
           <div class="meetup__aside">
-            <!-- meetup info -->
+            <meetup-info :date="date" :place="meetup.place" :organizer="meetup.organizer" />
           </div>
         </div>
       </div>
