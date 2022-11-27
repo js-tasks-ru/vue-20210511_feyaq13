5b6808bc00d5c956eaf6a8de8f0d27e6da9d4172
feyaq13 <feyaq13@gmail.com>
2022-11-27 18:34:47 +0300

    добавляет решение задач 08-deep-vue

--- 08-deep-vue/02-NodesPagination/components/NodesPagination.vue
+++ 08-deep-vue/02-NodesPagination/components/NodesPagination.vue
@@ -1,5 +1,25 @@
 <script>
 export default {
   name: 'NodesPagination',
+
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+
+    perPage: {
+      type: Number,
+    },
+  },
+
+  render(h) {
+    let content = [];
+    if (this.$slots.default) {
+      content = this.$slots.default.slice(this.perPage * (this.page - 1), this.perPage * this.page);
+    }
+
+    return h('div', content);
+  },
 };
 </script>
