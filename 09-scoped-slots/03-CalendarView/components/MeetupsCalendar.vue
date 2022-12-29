01a6e7f429ee51fe274742c5c8e7d21814bf1f93
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-12-29 10:41:14 +0300

    Added task CalendarView

---
+++ 09-scoped-slots/03-CalendarView/components/MeetupsCalendar.vue
@@ -0,0 +1,64 @@
+<template>
+  <calendar-view>
+    <!-- Каждый митап - ссылка на страницу митапа -->
+    <!-- Используя слот требуется вывести список митапов дня в каждой ячейке -->
+    <!--
+    <router-link
+      :to="{ name: 'meetup', params: { meetupId: meetup.id } }"
+      class="rangepicker__event"
+      >{{ meetup.title }}</router-link
+    >
+    -->
+  </calendar-view>
+</template>
+
+<script>
+import CalendarView from './CalendarView';
+
+export default {
+  name: 'MeetupsCalendar',
+
+  components: {
+    CalendarView,
+  },
+
+  props: {
+    meetups: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.rangepicker__event {
+  --maxLines: 2;
+  --lineHeight: 16px;
+  --fontSize: 14px;
+
+  display: block;
+
+  text-align: left;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-size: var(--fontSize);
+  line-height: var(--lineHeight);
+  font-weight: 600;
+  color: var(--white);
+  padding: 4px 6px;
+  border-radius: 2px;
+  background-color: var(--blue);
+  text-decoration: none;
+  margin-top: 4px;
+}
+
+@media all and (min-width: 767px) {
+  .rangepicker__event {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    max-height: calc(var(--maxLines) * var(--lineHeight) + 6px);
+  }
+}
+</style>
