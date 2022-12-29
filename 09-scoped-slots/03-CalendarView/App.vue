9087bf105c7968ab87cd2f436050b46221f5cb26
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-12-29 10:41:14 +0300

    Added task CalendarView

---
+++ 09-scoped-slots/03-CalendarView/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page wrapper bg-grey" style="display: flex; flex-direction: row">
+    <div style="margin: 2rem; flex: 1 0">
+      <meetups-calendar v-if="meetups" :meetups="meetups" />
+    </div>
+
+    <div style="margin: 2rem; flex: 1 0">
+      <russian-holidays-calendar />
+    </div>
+  </div>
+</template>
+
+<script>
+import MeetupsCalendar from './components/MeetupsCalendar';
+import RussianHolidaysCalendar from './components/RussianHolidaysCalendar';
+import { fetchMeetups } from './data';
+
+export default {
+  name: 'App',
+  components: { RussianHolidaysCalendar, MeetupsCalendar },
+
+  data() {
+    return {
+      meetups: null,
+    };
+  },
+
+  async mounted() {
+    this.meetups = await fetchMeetups();
+  },
+};
+</script>
