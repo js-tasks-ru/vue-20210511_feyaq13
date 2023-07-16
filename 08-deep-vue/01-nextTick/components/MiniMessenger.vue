819cc34c8174195db33b06a07a3fc0b781db394e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-07-16 23:43:53 +0300

    Added teacher solution for task: nextTick

---
+++ 08-deep-vue/01-nextTick/components/MiniMessenger.vue
@@ -0,0 +1,69 @@
+<template>
+  <main>
+    <messages-list ref="messagesList" class="messages" :messages="messages" />
+    <form style="display: flex" @submit.prevent="send">
+      <input v-model="newMessage" type="text" placeholder="New message" />
+      <button>Send</button>
+    </form>
+  </main>
+</template>
+
+<script>
+import MessagesList from './MessegesList';
+
+let id = 0;
+
+export const TASK_SOLVED = true;
+
+export default {
+  name: 'MiniMessenger',
+
+  components: { MessagesList },
+
+  data() {
+    return {
+      newMessage: '',
+      messages: [
+        { id: id++, text: 'First message' },
+        { id: id++, text: 'Second message' },
+        { id: id++, text: 'Third message' },
+        { id: id++, text: 'Forth message' },
+      ],
+    };
+  },
+
+  methods: {
+    send() {
+      this.messages.push({
+        id: id++,
+        text: this.newMessage,
+      });
+      this.newMessage = '';
+
+      this.$nextTick().then(() => {
+        const element = this.$refs['messagesList'].$el;
+        element.scrollTop = element.scrollHeight - element.clientHeight;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+main {
+  font-family: sans-serif;
+  border: 1px solid #333;
+  width: 200px;
+  background-color: #efefef;
+}
+.messages {
+  padding: 0 1rem;
+  height: 300px;
+  overflow: auto;
+}
+input {
+  padding: 0.5rem;
+  width: 100%;
+  flex: 1 1 100%;
+}
+</style>
